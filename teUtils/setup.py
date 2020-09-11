48dd1253d2792c3d278350b65f1d1fc312bd36e0
Joseph Hellerstein <jlheller@uw.edu>
2020-09-11 09:23:11 -0700

    Remove fitting codes and tests from teUtils.

--- teUtils/setup.py
+++ teUtils/setup.py
@@ -27,7 +27,6 @@ setup(
     packages=['teUtils'],
     version=get_version("teUtils/_version.py"),   # Update the version number for new releases
     #scripts=['teutilities'],    # The name of your scipt, and also the command you'll be using for calling it
-    author='H M Sauro, J Hellerstein',
     author_email='hsauro@uw.edu',
     url='http://tellurium.analogmachine.org',
     description='Utilities for Tellurium',
@@ -48,7 +47,6 @@ setup(
         'pandas',
         'python-libsbml',
         'nose',
-        'docstring_expander',
     ],
     python_requires='>=3.6',
 )
