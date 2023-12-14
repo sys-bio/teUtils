5d94ebe4e5519de147438ffaf3d6401e82f6e2c4
Herbert Sauro <hsauro@u.washington.edu>
2023-12-14 15:35:37 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,8 @@ import sys
 from unittest import mock
 
 # Mock open3d because it fails to build in readthedocs
-MOCK_MODULES = ["tellurium", "random", "numpy", "matplotlib", "roadrunner", "copy"]
+MOCK_MODULES = ["tellurium", "random", "numpy", "mpl_toolkits", 
+                "matplotlib", "matplotlib.pyplot", "roadrunner", "copy"]
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
